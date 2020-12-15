f66d65051c2f4317ee03c388134378e83025eafa
Thiago Jorge <thiago.v.jorge@hotmail.com>
2020-12-15 14:53:35 -0300

    Added project

---
+++ AccessLog/Client/Classes/ApplicationConfigurations.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Classes
+{
+    public static class ApplicationConfigurations
+    {
+        public static readonly string PathLog = @"C:\dev\ProjetoPI\AccessLog\Client\Resources\access.log";
+ 
+    }
+}
---
+++ AccessLog/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Client.Classes;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadAccess(ApplicationConfigurations.PathLog);
+        }
+
+        public static void ReadAccess(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+        }
+    }
+}
