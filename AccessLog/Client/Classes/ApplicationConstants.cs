e7e0b8867ac2b332e8618332a9a15bc2504fbb64
Thiago Jorge <thiago.v.jorge@hotmail.com>
2020-12-21 14:02:36 -0300

    Get path

--- AccessLog/Client/Classes/ApplicationConstants.cs
+++ AccessLog/Client/Classes/ApplicationConstants.cs
@@ -2,6 +2,15 @@
 {
     public static class ApplicationConstants
     {
-        public static readonly string PathLog = @"C:\dev\ProjetoPI\AccessLog\Client\Resources\access.txt";
+        //public static readonly string PathLog = @"C:\dev\ProjetoPI\AccessLog\Client\Resources\access.txt";
+        public static readonly string PathLog = GetPath();
+        public static string GetPath()
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            string[] splitPath = path.Split("bin");
+            path = splitPath[0].ToString() + "Resources\\access.txt";
+
+            return path;
+        }
     }
 }
